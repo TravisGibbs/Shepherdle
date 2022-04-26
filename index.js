c92a3881010e0946f65731ac620192256035dee3
Travis Gibbs <28307522+TravisGibbs@users.noreply.github.com>
2022-04-26 12:33:46 -0400

    MOre improvements

--- index.js
+++ index.js
@@ -5,49 +5,57 @@ const data = {
         "special": "General",
         "related": ["George O'Malley", "Derek Shepherd", "Nathan Riggs", "Andrew DeLuca", "Atticus Lincoln"],
         "family": ["Lexie Grey", "Maggie Pierce", "Ellis Grey"],
-        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/736747/1185da6c47d2666bd36bc72538600150/330x330-Q90_1185da6c47d2666bd36bc72538600150.jpg"
     },
     "Cristina Yang": {
         "special": "Cardio",
         "related": ["Preston Burke", "Owen Hunt", "Shane Ross"],
         "family": [""],
-        "seasons":[1,2,3,4,5,6,7,8,9,10]
+        "seasons":[1,2,3,4,5,6,7,8,9,10],
+        "img": "https://64.media.tumblr.com/6f320f1c6492a9f218149aebd018b924/8c76e426af3d4b0b-17/s640x960/83de4f5e7e736e4eadd091b6941b3ba2b21d4ec6.png"
     },
     "Izzie Stevens": {
         "special": "Intern",
         "related": ["Alex Karev", "George O'Malley"],
         "family": [""],
-        "seasons":[1,2,3,4,5,6]
+        "seasons":[1,2,3,4,5,6],
+        "img": "https://i.pinimg.com/originals/c0/ff/af/c0ffaf7916530e94350d043a4e73720c.jpg"
     },
     "George O'Malley": {
         "special": "Intern",
         "related": ["Izzie Stevens", "Meredith Grey", "Callie Torres"],
         "family": [""],
-        "seasons":[1,2,3,4,5]
+        "seasons":[1,2,3,4,5],
+        "img": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/greys-anatomy-george-o-malley-1587798156.jpg?crop=0.566xw:1.00xh;0.204xw,0&resize=480:*"
     },
     "Miranda Bailey": {
         "special": "General",
         "related": ["Benjamin Warren"],
         "family": [""],
-        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/736747/1185da6c47d2666bd36bc72538600150/330x330-Q90_1185da6c47d2666bd36bc72538600150.jpg"
     },
     "Richard Webber": {
         "special": "General",
         "related": ["Catherine Fox", "Ellis Grey"],
         "family": ["Maggie Pierce"],
-        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/736314/fb82b61037942db4a7684377fa538764/330x330-Q90_fb82b61037942db4a7684377fa538764.jpg"
     },
     "Alex Karev": {
         "special": "Peds",
         "related": ["Izzie Stevens", "Jo Wilson", "Lexie Grey", "Addison Montgomery", "Heather Brooks"],
         "family": [""],
-        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        "seasons":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+        "img": "https://static.wikia.nocookie.net/my-high-school-musical-series/images/c/c6/AlexKarevS104.jpg/revision/latest/top-crop/width/360/height/450?cb=20201223043758"
     },
     "Preston Burke": {
         "special": "Cardio",
         "related": ["Cristina Yang"],
         "family": [""],
-        "seasons":[1,2,3]
+        "seasons":[1,2,3],
+        "img": "https://openpsychometrics.org/tests/characters/test-resources/pics/GA/8.jpg"
     },
     "Derek Shepherd": {
         "special": "Nuero",
@@ -89,7 +97,8 @@ const data = {
         "special": "Trauma",
         "related": ["Cristina Yang", "Amelia Shepherd", "Teddy Altman", "Carina DeLuca"],
         "family": [""],
-        "seasons":[5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        "seasons":[5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/736363/28eb23c13ebc5d3773860dffb9dd5f97/330x330-Q90_28eb23c13ebc5d3773860dffb9dd5f97.jpg"
     },
     "Arizona Robbins": {
         "special": "OB",
@@ -101,7 +110,8 @@ const data = {
         "special": "Cardio",
         "related": ["Owen Hunt", "Thomas Koracick", "Mark Sloan"],
         "family": [""],
-        "seasons":[6,7,8,15,16,17,18]
+        "seasons":[6,7,8,15,16,17,18],
+        "img":"https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/2169675/9cd7169fcd34c51471bce1fa37cc50b2/330x330-Q90_9cd7169fcd34c51471bce1fa37cc50b2.jpg"
     },
     "April Kepner": {
         "special": "Trauma",
@@ -119,7 +129,8 @@ const data = {
         "special": "General",
         "related": ["Alex Karev", "Jackson Avery", "Paul Stadler", "Atticus Lincoln"],
         "family": [""],
-        "seasons":[10,11,12,13,14,15,16,17,18]
+        "seasons":[10,11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/736806/12d2d1239ecb5ac48576258697231fb8/330x330-Q90_12d2d1239ecb5ac48576258697231fb8.jpg"
     },
     "Shane Ross": {
         "special": "Cardio",
@@ -143,13 +154,15 @@ const data = {
         "special": "Nuero",
         "related": ["Owen Hunt", "Thomas Koracick"],
         "family": ["Derek Shepherd"],
-        "seasons":[11,12,13,14,15,16,17,18]
+        "seasons":[11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/753071/46e4f3eb2238cf20ed1866e2dcd971f1/330x330-Q90_46e4f3eb2238cf20ed1866e2dcd971f1.jpg"
     },
     "Maggie Pierce": {
         "special": "Cardio",
         "related": ["Andrew DeLuca", "Jackson Avery"],
         "family": ["Richard Webber", "Lexie Grey", "Meredith Grey", "Ellis Grey"],
-        "seasons":[11,12,13,14,15,16,17,18]
+        "seasons":[11,12,13,14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/753040/2077280daba6c87039d61f8b7f01e53e/330x330-Q90_2077280daba6c87039d61f8b7f01e53e.jpg"
     },
     "Benjamin Warren": {
         "special": "Intern",
@@ -179,7 +192,8 @@ const data = {
         "special": "Ortho",
         "related": ["Meredith Grey", "Jo Wilson"],
         "family": [""],
-        "seasons":[16, 17, 18]
+        "seasons":[16, 17, 18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/2848432/11191b3062e3e64e432b196c87219542/330x330-Q90_11191b3062e3e64e432b196c87219542.jpg"
     },
     "Ellis Grey": {
         "special": "General",
@@ -221,7 +235,8 @@ const data = {
         "special": "Intern",
         "related": ["Nico Kim"],
         "family": [],
-        "seasons":[14,15,16,17,18]
+        "seasons":[14,15,16,17,18],
+        "img": "https://cdn1.edgedatg.com/aws/v2/abc/GreysAnatomy/person/2848454/a3c33febb320197ad6c97057119d626f/330x330-Q90_a3c33febb320197ad6c97057119d626f.jpg"
     },
     "Nicole Herman": {
         "special": "OB",
@@ -317,9 +332,9 @@ function newGuess(name) {
     }
 
     var row = $("<tr></tr>")
-    var na = $("<th class='"+color1+"'>"+name+"</th>")
-    var spe = $("<th class='"+color2+"'>"+guess.special+"</th>")
-    var sea = $("<th class='"+color3+"'>"+guess.seasons.toString()+"</th>")
+    var na = $("<th class='font "+color1+"'>"+name+"</th>")
+    var spe = $("<th class='font "+color2+"'>"+guess.special+"</th>")
+    var sea = $("<th class='font "+color3+"'>"+guess.seasons.toString()+"</th>")
     row.append(na)
     row.append(spe)
     row.append(sea)
@@ -329,10 +344,14 @@ function newGuess(name) {
 $(document).ready(function(){
     $( "#answer_l").text("The correct answer was " + randomChar + ".")
     $( "#answer_w").text(randomChar + " was the correct answer!")
+    
     if (window.innerWidth/2 < 650) {
         width = innerWidth
+        $('#form-con').removeClass('web-width')
+        $('#form-con').addClass('mobile-width')
     } else {
         width =  window.innerWidth/2
+        $('#enter').toggleClass("hidden")
     }
     $( "#win" ).dialog({
         autoOpen: false,
@@ -370,4 +389,5 @@ $(document).ready(function(){
             width: width
         });
     });
+    
 })
